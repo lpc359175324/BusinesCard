991748868cf5617484b14e729d4a2be0163f324b
lpc <18347457687@163.com>
2018-07-22 20:04:36 +0800

    第一次提交

---
+++ src/main/java/com/businesscard/bill/BusinessApplication.java
@@ -0,0 +1,12 @@
+package com.businesscard.bill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessApplication {
+
+        public static void main(String[] args) {
+                SpringApplication.run(BusinessApplication.class, args);
+        }
+}
